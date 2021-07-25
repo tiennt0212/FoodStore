9083332daf069e98d2ca7f050af3708cb788bd61
tiennt0212@gmail.com <leulue@example.com>
2021-07-25 02:56:47 -0700

    Create Project

---
+++ backend-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0e12366d6c6513861bf5cdf945873d93'),
+    },
+  },
+});
---
+++ backend-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6155aa98-436a-46d7-9408-df926409f15c'
+};
